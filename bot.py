a91993d47e34eb50037741e3403531b346e88a83
vimeu <devil707n@gmail.com>
2026-04-17 04:20:37 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,26 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "8412718462:AAGfYzR2mWzJWjMamQEn-uPTxgo0FP5m2uM"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("تلحس كسي")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling() 
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("8412718462:AAGfYzR2mWzJWjMamQEn-uPTxgo0FP5m2uM")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("ها اخ الكحبه خليتني اشتغل 24 ساعه شنو عبد يمك اني")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
